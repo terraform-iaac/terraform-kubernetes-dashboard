dde7511923668114aaa777f9441b0080b1b060ee
bohdantverdyi <ownedd777@gmail.com>
2020-07-13 14:57:12 +0300

    Upgrade default helm chart version

--- variables.tf
+++ variables.tf
@@ -51,5 +51,5 @@ variable "enable_skip_button" {
 variable "chart_version" {
   description = "Helm Chart version (Not Recomended)"
   type        = string
-  default     = "2.0.1"
+  default     = "2.2.0"
 }
