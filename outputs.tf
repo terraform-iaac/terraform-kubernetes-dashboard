07343b179894bf769acb029ae17184d37ef1bc22
Bohdan Tverdyi <ownedd777@gmail.com>
2021-01-20 16:07:45 +0200

    Add new output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "admin_token" {
   value = var.create_admin_token ? lookup(data.kubernetes_secret.admin_token[0].data, "token") : "Not enabled"
 }
+output "admin_service_account" {
+  value = local.dashboard_admin_service_account
+}
 output "namespace" {
   value = var.namespace
 }
