c26296dff5ab45af055c0d631b557c586af8aab4
Dmytro Ostapenko <d.ostapenko97@live.com>
2024-02-15 19:46:34 +0200

    Update kubernetes dashboard to kube v1.24 requirements

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "admin_token" {
-  value = var.create_admin_token ? lookup(data.kubernetes_secret.admin_token[0].data, "token") : "Not enabled"
+  value = var.create_admin_token ? lookup(kubernetes_secret_v1.admin_token[0].data, "token") : "Not enabled"
 }
 output "admin_service_account" {
-  value = local.dashboard_admin_service_account
+  value = kubernetes_service_account_v1.admin_service_account[0].metadata[0].name
 }
 output "namespace" {
-  value = var.namespace
+  value = var.create_namespace ? kubernetes_namespace.namespace[0].id : var.namespace
 }
 output "dashboard_url" {
   value = "${var.dashboard_subdomain}${var.domain}"
